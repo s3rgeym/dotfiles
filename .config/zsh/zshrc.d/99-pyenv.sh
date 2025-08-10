c0bb1a9ec3eef5042d4e092849d3dd00b42613aa
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2025-08-10 15:12:39 +0300

    добавил прозрачность foot через kwin

--- .config/zsh/zshrc.d/99-pyenv.sh
+++ .config/zsh/zshrc.d/99-pyenv.sh
@@ -1,6 +1,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 if [ -n "$(command -v pyenv)" ]; then
-  #echo "Initialize pyenv..."
+  #echo "Initialize pyenv..." >&2
   eval "$(pyenv init - zsh)"
 fi
