9f18617a1a0cfd0d9d2ab22205cb4e7f03986163
Sergey M <tz.4678@gmail.com>
2026-02-17 14:03:52 +0300

    refac

--- .config/nvim/lua/user/utils.lua
+++ .config/nvim/lua/user/utils.lua
@@ -1,19 +1,4 @@
 local M = {}
-
-function M.set_keymaps(t)
-  for mode, maps in pairs(t) do
-    for _, m in ipairs(maps) do
-      local opts = { desc = m[3] }
-      for k, v in pairs(m) do
-        if type(k) == "string" then
-          opts[k] = v
-        end
-      end
-      vim.keymap.set(mode, m[1], m[2], opts)
-    end
-  end
-end
-
 function M.find_project_root()
   local root_markers = {
     ".git",
@@ -29,5 +14,20 @@ function M.find_project_root()
   }
   return vim.fs.root(0, root_markers)
 end
+
+function M.set_keymaps(t)
+  for mode, maps in pairs(t) do
+    for _, m in ipairs(maps) do
+      local opts = { desc = m[3] }
+      for k, v in pairs(m) do
+        if type(k) == "string" then
+          opts[k] = v
+        end
+      end
+      vim.keymap.set(mode, m[1], m[2], opts)
+    end
+  end
+end
+
 
 return M
